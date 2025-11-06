dcca415a952cf502119b1bba86861d5c9bff34a9
7Bssk7 <arhip.fin3@gmail.com>
2025-11-05 19:22:48 -0800

    Update

--- 210-lab-29.cpp
+++ 210-lab-29.cpp
@@ -13,12 +13,22 @@ using namespace std;
 
 class Car{
     public:
+        Car (){};
         Car(string m, int mile, int y, double p){
             make = m;
             mileage = mile;
             year = y;
             price = p;
         }
+
+        string getMake () const{ return make;}
+        void setMake(string m) { make = m;}
+        int getMiles () const { return mileage;}
+        void setMiles(int mile) { mileage = mile;}
+        int getYear() const  { return year;}
+        void setYear(int y) { year = y;}
+        double getPrice() const { return price;}
+        void setPrice( double p) { price = p;}
 
     private:
         string make;
@@ -31,7 +41,7 @@ int main(){
 
     vector<Car> cars_s;
     vector<Car> cars_t;
-    string car;
+    string file_in;
     fstream car_sell("sell.txt");
     fstream car_trade("trade.txt");
 
@@ -41,6 +51,33 @@ int main(){
     else if(!car_trade){
         return 1;
     }
+
+    while(getline(car_sell, file_in)){
+        Car car_temp;
+        stringstream ss(file_in);
+        string make;
+        int mileage;
+        int year;
+        double price;
+
+        ss >> make >> mileage >> year >> price;
+
+        car_temp.setMake(make);
+        car_temp.setMiles(mileage);
+        car_temp.setYear(year);
+        car_temp.setPrice(price);
+
+        cars_s.push_back(car_temp);
+
+    }
+
+    car_sell.close();
+
+    while(getline(car_trade, file_in)){
+
+    }
+
+    car_trade.close();
 
     
 

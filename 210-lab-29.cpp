b1a17e015dd951d854a2bc21b36b01f633bccfd6
7Bssk7 <arhip.fin3@gmail.com>
2025-11-05 18:56:21 -0800

    Update

--- 210-lab-29.cpp
+++ 210-lab-29.cpp
@@ -1,12 +1,19 @@
 // COMSC-210 | Lab 29 | Arkhip Finski
 // IDE used: Visual Studio
 #include <iostream>
+#include <fstream>
 #include <map>
 #include <list>
+#include <random>
+#include <string>
+#include <vector>
 
 using namespace std;
 
 int main(){
+
+    vector<string> cars;
+
 
     return 0;
 }

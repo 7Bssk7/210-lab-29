517c12404aa5905f7e7894f88caf9647f56d1eaf
7Bssk7 <arhip.fin3@gmail.com>
2025-11-07 20:59:41 -0800

    Update 3

--- unit_test.cpp
+++ unit_test.cpp
@@ -1,3 +1,32 @@
 // COMSC-210 | Lab 30 Unit Test | Arkhip Finski
 // IDE used: Visual Studio
 
+#include <iostream>
+#include <vector>
+#include <list>
+#include <string>
+using namespace std;
+
+class Car{
+    public:
+        Car(string m, string mo, int mile, int y, double p){
+            make = m;
+            model = mo;
+            mileage = mile;
+            year = y;
+            price = p;
+        }
+
+        string getMake() const { return make;}
+        string getModel() const { return model;}
+        int getMile() const { return mileage;}
+        int getYear() const { return year;}
+        double getPrice() const { return price;}
+
+    private:
+        string make;
+        string model;
+        int mileage;
+        int year;
+        double price;
+};
